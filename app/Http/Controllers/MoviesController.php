5ef57eeb72a249e032b113644751fd820994b9c6
EvgenSidorov <wnw05@yandex.ru>
2021-09-06 19:07:58 +0300

    добавили фильтр по названию

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -9,8 +9,13 @@ use Illuminate\Http\Request;
 class MoviesController extends Controller
 {
     public function index(){
-        $movies = Movie::paginate(12);
+        $movies = Movie::where('is_active', true)
+            ->when(request()->has('query'), function($q){
+                return $q->where('title', 'like', '%'.request('query').'%');
+            })
+            ->paginate(12);
 //        dd($movies);
+
         return view('movies.index', [
             'movies' => $movies,
             'title' => 'All films'
@@ -23,10 +28,16 @@ class MoviesController extends Controller
             'title' => $movie->title
         ]);
     }
+
 }
 
 
 // добавить Н1 заголовок для всех страниц должен передаваться из контроллера в шаблон
 //  вывести в хедере ссылки на главную страницу и на каталог фильмов
 //  используя метод route()
-// сделать шаблон фильма карточка фильма
+// сделать шаблон фильма карточка фильма вывести дескриптион
+// сделать форму поиска
+// сделать фильтр по цене
+// сделать поиск по цене min max https://jqueryui.com/slider/#range
+
+
