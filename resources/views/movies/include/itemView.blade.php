d8d20763ffd2eb7a58572d21ca3ee50492506741
EvgenSidorov <wnw05@yandex.ru>
2021-09-14 12:28:43 +0300

    add card chenges

--- resources/views/movies/include/itemView.blade.php
+++ resources/views/movies/include/itemView.blade.php
@@ -4,28 +4,31 @@
         </div>
         <div class="col-4 bg-light" style="">
             <div class=" d-flex m-3 justify-content-center" style="min-height: 70px;">
-                <h3 class="card-title align-self-center">{{$movie->title}}</h3>
+                <h4 class="card-title align-self-center">{{$movie->title}}</h4>
             </div>
             <p class="card-text ">
-            <div class="text bold mb-3">Rating: {{$rating}}</div>
+            <div class="bg-secondary text-white text-center bold mb-3">Rating: {{$rating}}</div>
             {{$movie->description}}
             </p>
             <div class="d-flex mt-3">
                 Your price: <p class="card-text mx-3">{{number_format($movie->price, 2)}} $</p>
             </div>
-            <div class="d-flex mt-5 justify-content-between">
-                <a href="#" class="btn btn-outline-dark">add count</a>
+            <div class="d-flex mt-5 justify-content-around">
+                <a href="#" class="btn btn-outline-success">+</a>
                 <h5 class="card-text text-center">Count: 1</h5>
-                <a href="#" class="btn btn-outline-dark">remove count</a>
+                <a href="#" class="btn btn-outline-danger">-</a>
+            </div>
+            <div class="d-flex mt-5 justify-content-around">
+                <a href="#" class="btn btn-outline-success">Add to Cart</a>
             </div>
 
         </div>
     </div>
     <div class="row">
         <div class="col-8">
-            <div class="d-flex justify-content-around m-5">
-                <a href="#" class="btn btn-outline-dark">Add to cart</a>
-                <a href="{{ route('app.movies.index') }}" class="btn btn-outline-dark">Go back</a>
+            <div class="d-flex m-5">
+
+                <a href="{{ route('app.movies.index') }}" class="btn btn-outline-dark"><-Back</a>
             </div>
         </div>
     </div>
