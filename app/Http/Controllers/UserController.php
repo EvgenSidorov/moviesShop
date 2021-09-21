b7b2229fb01e22f9414e3bade8a966b204204031
EvgenSidorov <wnw05@yandex.ru>
2021-09-21 14:50:58 +0300

    add the first iteration sign Up form and so add first test user in table users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
     public function signUp(){
         return view('auth.sign-up');
+    }
+
+    public  function signUpSubmit(ContactRequest $req){
+        $user = new User();
+        $user->name = $req->input('name');
+        $user->email = $req->input('email');
+        $user->phone = $req->input('phone');
+        $user->password = $req->input('password');
+        $user->save();
+        return redirect()->route('app.home');
     }
 }
